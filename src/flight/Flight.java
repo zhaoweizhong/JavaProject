222ad40cc59c1de3e27d441bdc442ae0b18b5122
Zhaowei Zhong <zhong_zhaowei@163.com>
2017-05-12 20:58:05 +0800

    Fifth Commit

--- src/flight/Flight.java
+++ src/flight/Flight.java
@@ -3,18 +3,52 @@ package flight;
 import user.Passenger;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Flight {
-    private static int flightID = 0;
+    private static int flightQuantity = 0;
+    private int flightID;
     private String flightNumber;
     private String departureTime;
     private String arrivalTime;
     private String departureAirport;
     private String arrivalAirport;
     private int price;
-    private int seatCapacity;
     private int[] seatBooked;
     private FlightStatus flightStatus;
     private ArrayList<Passenger> passengers;
 
+    public Flight(String flightNumber, String departureTime, String arrivalTime, String departureAirport, String arrivalAirport, int price) {
+        passengers = new ArrayList<>();
+        this.flightNumber = flightNumber;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.price = price;
+        this.flightStatus = FlightStatus.UNPUBLISHED;
+        flightQuantity++;
+        flightID = flightQuantity;
+        seatBooked = new int[0];
+
+    }
+
+
+
+    public int[] getSeatBooked() {
+        return seatBooked;
+    }
+
+    public void addSeatBooked(int seatNumber) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<seatBooked.length; i++) {
+            list.add(seatBooked[i]);
+        }
+        list.add(seatNumber);
+        int[] newSeatBooked = new int[list.size()];
+        for(int i = 0;i<list.size();i++){
+            newSeatBooked[i] = list.get(i);
+        }
+        seatBooked = newSeatBooked;
+    }
 }
--- src/order/Order.java
+++ src/order/Order.java
@@ -1,32 +1,95 @@
 package order;
 
 import java.util.Date;
+import java.util.Random;
 
 import flight.Flight;
+import flight.SeatClass;
 import user.Passenger;
 
 public class Order {
 
     private Passenger passenger;
-    private int seat;
+    private SeatClass seatClass;
+    private int seatNumber;
     private Flight flight;
     private Date createDate;
     private OrderStatus status;
 
-    public Order(Passenger passenger, Flight flight, int seat) {
+    public Order(Passenger passenger, Flight flight, SeatClass seatClass) {
         this.passenger = passenger;
         this.flight = flight;
-        this.seat = seat;
-        createDate = new Date(); //now
+        this.seatClass = seatClass;
+        createDate = new Date(); // Now
         status = OrderStatus.UNPAID;
+        // Generate Seat Number by Random
+        Random random = new Random();
+        boolean flag = true;
+        if (seatClass == SeatClass.EconomyClass) {
+            do {
+                int num = random.nextInt(145) + 8;
+                for(int i=0;i<flight.getSeatBooked().length;i++){
+                    if(num == flight.getSeatBooked()[i]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    flight.addSeatBooked(num);
+                    seatNumber = num;
+                    break;
+                }
+            }while (1 == 1);
+        }else{
+            do {
+                int num = random.nextInt(9);
+                for(int i=0;i<flight.getSeatBooked().length;i++){
+                    if(num == flight.getSeatBooked()[i]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    flight.addSeatBooked(num);
+                    seatNumber = num;
+                    break;
+                }
+            }while (1 == 1);
+        }
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public String getSeat() {
+        String seat = null;
+        if (seatClass == SeatClass.EconomyClass) {
+            switch (seatNumber % 6) {
+                case 0: seat = (seatNumber / 6) + "A"; break;
+                case 1: seat = (seatNumber / 6) + "B"; break;
+                case 2: seat = (seatNumber / 6) + "C"; break;
+                case 3: seat = (seatNumber / 6) + "H"; break;
+                case 4: seat = (seatNumber / 6) + "J"; break;
+                case 5: seat = (seatNumber / 6) + "K"; break;
+            }
+        }else{
+            switch (seatNumber % 4) {
+                case 0: seat = (seatNumber / 4) + "A"; break;
+                case 1: seat = (seatNumber / 4) + "C"; break;
+                case 2: seat = (seatNumber / 4) + "H"; break;
+                case 3: seat = (seatNumber / 4) + "K"; break;
+            }
+        }
+        return seat;
     }
 
     public Passenger getPassenger() {
         return passenger;
     }
 
-    public int getSeat() {
-        return seat;
+    public SeatClass getSeatClass() {
+        return seatClass;
     }
 
     public Flight getFlight() {
